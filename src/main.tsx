5e60edd59a5535b88057f9e1471b5a532de30473
mykolao <mykolaoleskiv@gmail.com>
2022-12-06 02:04:09 +0200

    provide redux store

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { CssBaseline } from '@mui/material';
 import ThemeProvider from 'mui';
 import ReactDOM from 'react-dom/client';
+import StoreProvider from 'store';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -14,8 +15,10 @@ const rootElement = document.getElementById('root') as HTMLElement;
 ReactDOM.createRoot(rootElement).render(
   <StrictMode>
     <ThemeProvider>
-      <CssBaseline />
-      App
+      <StoreProvider>
+        <CssBaseline />
+        App
+      </StoreProvider>
     </ThemeProvider>
   </StrictMode>,
 );
