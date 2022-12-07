57fb8026643a08e630505c7a0e2daddb50c1cd25
mykolao <mykolaoleskiv@gmail.com>
2022-12-08 00:14:17 +0200

    add WeatherData type

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,12 @@ export interface CityInfo extends Geo {
   name: string;
   country: string;
 }
+
+export interface WeatherData {
+  temp: number;
+  humidity: number;
+  pressure: number;
+  feels_like: number;
+  description: string;
+  icon: string;
+}
