1b94701216e4634355108253df0f108e8d44c6b2
mykolao <mykolaoleskiv@gmail.com>
2022-12-07 23:54:46 +0200

    use customStyles helper

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,15 @@ import { FC, PropsWithChildren } from 'react';
 import { AppBar } from '@mui/material';
 import { Container, SxProps } from '@mui/system';
 import Logo from 'components/Logo';
+import SearchBar from 'components/SearchBar';
+import { customStyles } from 'mui/customStyles';
 
-const containerStyles: SxProps = {
+const containerStyles = customStyles({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   padding: '12px 8px',
-};
+});
 
 export const Header: FC<PropsWithChildren> = ({ children }) => {
   return (
