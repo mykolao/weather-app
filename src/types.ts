01def1622d0bf4d0bab33a4bfaf3b8880c6fb15f
mykolao <mykolaoleskiv@gmail.com>
2022-12-07 03:16:38 +0200

    decompose CityInfo type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,9 @@
-export interface CityInfo {
+export interface Geo {
+  lon: number;
+  lat: number;
+}
+
+export interface CityInfo extends Geo {
   name: string;
   country: string;
-  lat: number;
-  lon: number;
 }
